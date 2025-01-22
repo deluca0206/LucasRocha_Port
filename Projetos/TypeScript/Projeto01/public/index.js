c44254805243af2f20f4531847107635c7771272
Lucas Rocha <lucas0206@gmail.com>
2025-01-22 00:10:41 -0300

    Portfólio

---
+++ Projetos/TypeScript/Projeto01/public/index.js
@@ -0,0 +1,8 @@
+"use strict";
+let person = {
+    name: 'Lucas',
+    age: '33',
+    isAdmin: true
+};
+person.name = 'Lucas Rocha';
+console.log(person);
---
+++ Projetos/TypeScript/Projeto01/src/index.ts
@@ -0,0 +1,9 @@
+let person = {
+    name: 'Lucas',
+    age: '33',
+    isAdmin: true
+}
+
+person.name = 'Lucas Rocha'
+
+console.log(person);
