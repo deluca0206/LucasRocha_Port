d85abdee9562a5478efa7480002d3533fad9f6ea
Lucas Rocha <lucas0206@gmail.com>
2025-01-28 18:16:00 -0300

    Portfólio

--- Automações-Testes/Cypress/cypress/e2e/finances.cy.js
+++ Automações-Testes/Cypress/cypress/e2e/finances.cy.js
@@ -8,55 +8,22 @@ describe('Transações', () => {
 
     it('Cadastrar transações e Deleta todas as transações', () => {
 
-        criarTransacao("Freelancer 1", 250)
-        cy.get("tbody tr").eq(0).find("td.description").should("have.text", "Freelancer 1")
-        criarTransacao("Cinema", -50)
-        cy.get("tbody tr").eq(1).find("td.description").should("have.text", "Cinema")
-        criarTransacao("Freelancer 2", 150)
-        cy.get("tbody tr").eq(2).find("td.description").should("have.text", "Freelancer 2")
-        criarTransacao("Mercado", -100)
-        cy.get("tbody tr").eq(3).find("td.description").should("have.text", "Mercado")
-        criarTransacao("Freelancer 3", 100)
-        cy.get("tbody tr").eq(4).find("td.description").should("have.text", "Freelancer 3")
-
-        deletarTransacoes()
+        cy.criarTransacao("Freelancer 1", 250)
+        cy.criarTransacao("Cinema", -50)
+        cy.criarTransacao("Freelancer 2", 150)
+        cy.criarTransacao("Mercado", -100)
+        cy.criarTransacao("Freelancer 3", 100)
+        cy.deletarTransacoes()
     });
     it('Cadastra Transações e deleta transações especificas', () => {
-        criarTransacao("Freelancer 1", 250)
-        cy.get("tbody tr").eq(0).find("td.description").should("have.text", "Freelancer 1")
-        criarTransacao("Cinema", -50)
-        cy.get("tbody tr").eq(1).find("td.description").should("have.text", "Cinema")
-        criarTransacao("Freelancer 2", 150)
-        cy.get("tbody tr").eq(2).find("td.description").should("have.text", "Freelancer 2")
-        criarTransacao("Mercado", -100)
-        cy.get("tbody tr").eq(3).find("td.description").should("have.text", "Mercado")
-        criarTransacao("Freelancer 3", 100)
-        cy.get("tbody tr").eq(4).find("td.description").should("have.text", "Freelancer 3")
-
-
-        deletarTransacoesEsp("Cinema")
-        deletarTransacoesEsp("Mercado")
+        cy.criarTransacao("Freelancer 1", 250)
+        cy.criarTransacao("Cinema", -50)
+        cy.criarTransacao("Freelancer 2", 150)
+        cy.criarTransacao("Mercado", -100)
+        cy.criarTransacao("Freelancer 3", 100)
+        cy.deletarTransacoesEsp("Cinema")
+        cy.deletarTransacoesEsp("Mercado")
 
     });
     
 });
-
-function criarTransacao(descricao, valor) {
-  cy.contains("Nova Transação").click()
-  cy.get('#description').type(descricao)
-  cy.get('#amount').type(valor)
-  cy.get('#date').type("2023-02-15")
-  cy.contains("button", "Salvar").click()
-}
-
-function deletarTransacoes() {
-    for (let i = 0; i < 5; i++) {
-        cy.get('[data-index="0"] > :nth-child(4) > img').click();
-        cy.wait(1000)
-    }
-}
-
-function deletarTransacoesEsp(descricao) {
-        cy.contains('tr', descricao).find(':nth-child(4) > img').click();
-        cy.contains("tr", descricao).should("not.exist")
-}
--- Automações-Testes/Cypress/cypress/support/commands.js
+++ Automações-Testes/Cypress/cypress/support/commands.js
@@ -39,3 +39,23 @@ Cypress.Commands.add('lidarComOferta', ()=> {
             }
         });
 })
+
+Cypress.Commands.add('criarTransacao', (descricao, valor) => {
+    cy.contains("Nova Transação").click()
+    cy.get('#description').type(descricao)
+    cy.get('#amount').type(valor)
+    cy.get('#date').type("2023-02-15")
+    cy.contains("button", "Salvar").click()
+  })
+  
+  Cypress.Commands.add('deletarTransacoes', () => {
+      for (let i = 0; i < 5; i++) {
+          cy.get('[data-index="0"] > :nth-child(4) > img').click();
+          cy.wait(1000)
+      }
+  })
+  
+  Cypress.Commands.add('deletarTransacoesEsp',(descricao) => {
+          cy.contains('tr', descricao).find(':nth-child(4) > img').click();
+          cy.contains("tr", descricao).should("not.exist")
+  })
